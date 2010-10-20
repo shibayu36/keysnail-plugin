e8dea16b8b32b606a47c6d53375fb89917e0d2fa
shiba_yu36 <shibayu36@gmail.com>
2010-10-20 17:47:41 +0900

    とりあえずつくり始め

---
+++ tabgroup-screenlike.ks.js
@@ -0,0 +1,23 @@
+var PLUGIN_INFO =
+<KeySnailPlugin>
+    <name>TabGroup ScreenLike</name>
+    <name lang="ja">TabGruop ScreenLike</name>
+    <description>you can use TabGroupManager like screen</description>
+    <description lang="ja">タブグループマネージャをスクリーンのように使用することが出来ます。</description>
+    <version>0.1</version>
+    <updateURL></updateURL>
+    <iconURL></iconURL>
+    <author mail="shibayu36@gmail.com" homepage="http://d.hatena.ne.jp/shiba_yu36/">shiba_yu36</author>
+    <license>The MIT License</license>
+    <license lang="ja">MIT ライセンス</license>
+    <minVersion>1.0</minVersion>
+    <include>main</include>
+    <detail><![CDATA[
+=== Usage ===
+Nothing.
+    ]]></detail>
+    <detail lang="ja"><![CDATA[
+=== 使い方 ===
+なし。
+    ]]></detail>
+</KeySnailPlugin>;
