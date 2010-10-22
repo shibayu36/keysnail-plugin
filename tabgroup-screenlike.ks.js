9a839547f16e1b313a5ba1928e8b86c74c1c4222
shiba_yu36 <shibayu36@gmail.com>
2010-10-22 17:45:22 +0900

    数字を入力してそのグループに移動するやつ

--- tabgroup-screenlike.ks.js
+++ tabgroup-screenlike.ks.js
@@ -13,10 +13,11 @@ var PLUGIN_INFO =
     <minVersion>1.0</minVersion>
     <include>main</include>
     <provides>
-        <ext>tabgroup-select-left-group</ext>
-        <ext>tabgroup-select-right-group</ext>
-        <ext>tabgroup-open-new-group</ext>
-        <ext>tabgroup-close-current-group</ext>
+        <ext>tabgroup-previous</ext>
+        <ext>tabgroup-next</ext>
+        <ext>tabgroup-goto</ext>
+        <ext>tabgroup-create</ext>
+        <ext>tabgroup-close</ext>
     </provides>
     <detail><![CDATA[
 === Usage ===
@@ -28,28 +29,33 @@ Nothing.
     ]]></detail>
 </KeySnailPlugin>;
 
-ext.add("tabgroup-select-left-group", selectLeftGroup, M({
+ext.add("tabgroup-previous", selectPreviousGroup, M({
     ja : "左のグループを選択",
     en : "select left group"
 }));
 
-ext.add("tabgroup-select-right-group", selectRightGroup, M({
+ext.add("tabgroup-next", selectNextGroup, M({
     ja : "右のグループを選択",
     en : "select right group"
 }));
 
-ext.add("tabgroup-open-new-group", openNewGroup, M({
+ext.add("tabgroup-goto", selectNthGroup, M({
+    ja : "指定した番号のグループへ移動",
+    en : "select group of entered number"
+}));
+
+ext.add("tabgroup-create", openNewGroup, M({
     ja : "新しいグループを開く",
     en : "open new group"
 }));
 
-ext.add("tabgroup-close-current-group", closeCurrentGroup, M({
+ext.add("tabgroup-close", closeCurrentGroup, M({
     ja : "現在のグループを閉じる",
     en : "close current group"
 }));
 
 
-function selectLeftGroup () {
+function selectPreviousGroup () {
     if (KeySnail.windowType != "navigator:browser" || !("TabGroupsManager" in window))
         return;
 
@@ -57,12 +63,25 @@ function selectLeftGroup () {
     focusContent();
 }
 
-function selectRightGroup () {
+function selectNextGroup () {
     if (KeySnail.windowType != "navigator:browser" || !("TabGroupsManager" in window))
         return;
 
     TabGroupsManager.command.SelectRightGroup();
     focusContent();
+}
+
+function selectNthGroup () {
+    if (KeySnail.windowType != "navigator:browser" || !("TabGroupsManager" in window))
+        return;
+
+    prompt.read("goto group number: ", function (group_index) {
+        if (!group_index.match("\\d+"))
+            return;
+
+        TabGroupsManager.command.SelectNthGroup(group_index);
+        focusContent();
+    });
 }
 
 function openNewGroup () {
@@ -83,3 +102,4 @@ function focusContent() {
     getBrowser().focus();
     _content.focus();
 }
+
