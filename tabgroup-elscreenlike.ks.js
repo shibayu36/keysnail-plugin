618255f327f9858f115c48b185f6e44a70caefdc
shiba_yu36 <shibayu36@gmail.com>
2011-09-20 14:08:30 +0900

    fix

--- tabgroup-elscreenlike.ks.js
+++ tabgroup-elscreenlike.ks.js
@@ -147,8 +147,8 @@ function  gotoLastSelectedGroup () {
 }
 
 function focusContent() {
-    getBrowser().focus();
-    // _content.focus();
+    // getBrowser().focus();
+    _content.focus();
 }
 
 function saveCurrentGroupIndex () {
